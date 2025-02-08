a4358891d1f4511929b9cbd93601741e7766d2da
abhishekbadole12 <abhishekbadole12@gmail.com>
2025-02-08 22:16:26 +0530

    feat: rotation

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,18 +3,31 @@ import HeroStyles from "./Hero.module.css";
 export default function Hero() {
   return (
     <section className={HeroStyles.Hero}>
-      <div className={HeroStyles.edgeShade}>
-        {/* Content */}
-        <div className={HeroStyles.mainContent}>
-          <h2 className={HeroStyles.title}>
-            Adding 10+ healthy <br/> years to your life
-          </h2>
-          <h6 className={HeroStyles.subTitle}>join the revolution</h6>
+      {/* Rotating SVG Container */}
+      <div className={HeroStyles.rotatingContainer}>
+        {/* Outer */}
+        <div className={HeroStyles.outerSvg}>
+          {/* Middle */}
+          <div className={HeroStyles.middleSvg}>
+            {/* Inner */}
+            <div className={HeroStyles.innerSvg} />
+          </div>
         </div>
       </div>
+
+      {/* Fixed Content (Text) */}
+      <div className={HeroStyles.mainContent}>
+        <h2 className={HeroStyles.title}>
+          Adding 10+ healthy <br /> years to your life
+        </h2>
+        <h6 className={HeroStyles.subTitle}>join the revolution</h6>
+      </div>
+
+      {/* Top Shade */}
+      <div className={HeroStyles.topShade} />
+      
+      {/* Bottom Shade */}
+      <div className={HeroStyles.bottomShade} />
     </section>
   );
-}
-{
-  /* <div className={HeroStyles.edgeShade}/> */
 }
